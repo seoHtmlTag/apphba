7aadde4c8791ac3898de9530c2ee4e47a1806891
seoHtmlTag <139620971+seoHtmlTag@users.noreply.github.com>
2025-03-16 18:01:22 +0200

    Add files via upload

--- background.js
+++ background.js
@@ -1,3 +1,11 @@
-chrome.action.onClicked.addListener((tab) => {
-  chrome.tabs.update(tab.id, { url: 'https://seohtmltag.github.io/apphba' });
+// chrome.action.onClicked.addListener((tab) => {
+//   chrome.tabs.update(tab.id, { url: 'https://seohtmltag.github.io/apphba' });
+// });
+
+chrome.app.runtime.onLaunched.addListener(() => {
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    if (tabs.length > 0) {
+      chrome.tabs.update(tabs[0].id, { url: 'https://seohtmltag.github.io/apphba' });
+    }
+  });
 });
